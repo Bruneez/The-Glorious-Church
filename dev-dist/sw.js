59975e10e8b5d59f33b73e6c1fd4a82c5102cff1
bruneez <apollisbruneez@gmail.com>
2026-07-27 19:13:51 +0200

    Fix Recharts dependency and standardize on npm

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uaflhj2vjh8"
+    "revision": "0.fvcjj254vro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
