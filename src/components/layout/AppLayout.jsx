66de62bc5684b3d86c5802d62b57a77e6f6dd526
bruneez <apollisbruneez@gmail.com>
2026-08-08 19:04:12 +0200

    enlarge map

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import SidebarBackdrop from './SidebarBackdrop';
 import Sidebar from './Sidebar';
 import SidebarBrand from './SidebarBrand';
@@ -8,6 +8,8 @@ import { useMobileMenu } from '@/hooks/useMobileMenu';
 
 export default function AppLayout() {
   const mobileMenu = useMobileMenu();
+  const { pathname } = useLocation();
+  const isMapPage = pathname === '/map';
 
   useStaffLastSeen();
 
@@ -20,17 +22,28 @@ export default function AppLayout() {
       />
       <SidebarBackdrop isOpen={mobileMenu.isOpen} onClose={mobileMenu.close} />
 
-      <div className="flex flex-1 min-h-0 flex-col xl:grid xl:grid-cols-[17.5rem_1fr] xl:grid-rows-[4.5rem_minmax(0,1fr)] overflow-hidden">
-        <PageHeader />
+      <div
+        className={`flex flex-1 min-h-0 flex-col overflow-hidden xl:grid xl:grid-cols-[17.5rem_1fr] ${
+          isMapPage ? 'xl:grid-rows-[minmax(0,1fr)]' : 'xl:grid-rows-[4.5rem_minmax(0,1fr)]'
+        }`}
+      >
+        {!isMapPage ? <PageHeader /> : null}
 
         <Sidebar
           isMobileOpen={mobileMenu.isOpen}
           onCloseMobile={mobileMenu.close}
           drawerRef={mobileMenu.drawerRef}
           labelId={mobileMenu.labelId}
+          fullHeight={isMapPage}
         />
 
-        <main className="min-h-0 min-w-0 overflow-y-auto overflow-x-hidden flex-1 xl:row-start-2 xl:col-start-2 px-4 py-4 sm:px-5 xl:px-7 xl:py-6 2xl:px-8 w-full max-w-none">
+        <main
+          className={`min-h-0 min-w-0 w-full max-w-none flex-1 xl:col-start-2 ${
+            isMapPage
+              ? 'h-full overflow-hidden p-0 xl:row-start-1'
+              : 'overflow-y-auto overflow-x-hidden px-4 py-4 sm:px-5 xl:row-start-2 xl:px-7 xl:py-6 2xl:px-8'
+          }`}
+        >
           <Outlet />
         </main>
       </div>
